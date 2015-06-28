a06962a04b75db442b51bef50d923974628e7b88
Mr-Halo <shihao_mac@163.com>
2015-06-28 11:20:44 +0800

    Add description of Circle internal header

--- Circle.h
+++ Circle.h
@@ -1,9 +1,35 @@
+/*
+*Description:Circle fucntion internal Header
+*Date:2015-06-28
+*Author:SH
+*/
+
+/*Foreign headers*/
 #include <GL/glut.h>
 
+/*
+*Description:Function used for drawing Circle
+*@return:void
+*@parameter:float 
+*/
 void Circle(GLfloat r);
-
+/*
+*Description:Function used for preparation of drawing
+*@return:void
+*@parameter:void
+*/
 void display(void);
 
+/*
+*Description:Initialization fucntion
+*@return:void
+*@parameter:void
+*/
 void init(void);
 
+/*
+*Description:Main function
+*@return:int
+*@parameter: int,char
+*/
 int main(int argc,char **argv);
